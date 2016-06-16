670aede794295144c6b6265ee34911e4f586a247
Jakob R. K <jrk@touchlogic.dk>
2016-06-16 15:12:43 +0200

    removed rects and added targetMatches

--- poelsefabrik.py
+++ poelsefabrik.py
@@ -38,29 +38,36 @@ for the_file in os.listdir(resultPath):
 
 cap = cv2.VideoCapture(videoPath)
 frameCounter = 0
+matches = 0
+targetMatches = 5
 
 faceCascade = cv2.CascadeClassifier(cascPath)
 eyesCascade = cv2.CascadeClassifier("haarcascade_eye.xml")
 
-while (cap.isOpened() and frameCounter < 100):
+while (cap.isOpened() and matches < targetMatches):
     frameCounter += 1
     ret, frame = cap.read()
 
     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
     # Detect faces in the image
     faces = getFaces(faceCascade, gray)
-
+    goodFaces = 0
     if len(faces) > 0:
         print "Found {0} faces in frame".format(len(faces))
         for (x, y, w, h) in faces:
-            cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
+            #cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
             faceCropGray = gray[y:y+h,x:x+w]
-            roi_color = frame[y:y+h,x:x+w]
+            faceCropColor = frame[y:y+h,x:x+w]
             eyes = eyesCascade.detectMultiScale(faceCropGray)
-            for (ex,ey,ew,eh) in eyes:
-                cv2.rectangle(roi_color,(ex,ey),(ex+ew,ey+eh),(0,255,0),2)
+            if(len(eyes) == 2):
+                #TODO figure out if the eyes are are next to each other...
+                goodFaces += 1
+            
+            #for (ex,ey,ew,eh) in eyes:
+            #    cv2.rectangle(faceCropColor,(ex,ey),(ex+ew,ey+eh),(0,255,0),2)
 
-        cv2.imwrite("faces_result/f_{0}.png".format(frameCounter), frame)
+        if(goodFaces > 0):
+            cv2.imwrite("faces_result/f_{0}.png".format(frameCounter), frame)
     else:
         print "No faces in frame {0}".format(frameCounter)
     for x in xrange(0, 24):
